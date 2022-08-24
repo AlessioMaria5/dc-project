15802e9646faf39c555ead63d4f3a7c34748d190
AlessioMaria5 <alessiomaria5@gmail.com>
2022-08-24 17:57:02 +0200

    push ( da fixare img ) solo html

--- src/components/dc-header.vue
+++ src/components/dc-header.vue
@@ -1,16 +1,89 @@
 <template>
- <div class="d-cheader">
-     My Header
+ <div class="dc-header">
+     <div class="head-logo">
+         <img src="../assets/img/dc-logo.png" alt="DC Comics Logo">
+     </div>
+     <div class="nav">
+         <ul v-for="(link, index) in links" :key="index">
+             <li><a :href="link.url" :class ="link.active = true?'active':''" class="upperText">{{link.text}}</a></li>
+         </ul>
+     </div>
  </div>
 </template>
 
 <script>
 export default {
 
-name: 'DCheader'
+    name: 'DCheader',
+    data() {
+        return {
+            links: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    active: false
+                },
+
+                {
+                    text: 'Comics',
+                    url: '#',
+                    active: true
+                },
+
+                {
+                    text: 'Movies',
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    active: false
+                },
+
+                {
+                    text: 'Games',
+                    url: '#',
+                    active: false
+                },
+
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    active: false
+                },
+
+                {
+                    text: 'Videos',
+                    url: '#',
+                    active: false
+                },
+
+                {
+                    text: 'Fans',
+                    url: '#',
+                    active: false
+                },
+
+                {
+                    text: 'News',
+                    url: '#',
+                    active: false
+                },
+
+                {
+                    text: 'Shop',
+                    url: '#',
+                    active: false
+                },
+            ]
+        }
+    }
 }
+
+
 </script>
 
-<style>
+<style lang="scss">
 
 </style>

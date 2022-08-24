25828fc55671792cc5b0588235e5598b03f33878
AlessioMaria5 <alessiomaria5@gmail.com>
2022-08-24 18:18:37 +0200

    risolto bug img

--- src/components/dc-shop.vue
+++ src/components/dc-shop.vue
@@ -2,7 +2,7 @@
   <div class="dc-shop">
       <ul v-for="(link, index) in links" :key="index">
             <li>
-                <img :src="link.img" alt="DC shop">   
+                <img :src="link.img" alt="dc shop">   
                 <a :href="link.url" :class ="link.active = true?'active':''" class="upperText">{{link.text}}</a>
             </li>
          </ul>
@@ -19,48 +19,48 @@ export default {
         return {
             links: [
                 {
-                    img: '@/assets/img/buy-comics-digital-comics.png',
+                    img: require('../assets/img/buy-comics-digital-comics.png'),
                     text: 'Digital Comics',
                     url: '#',
                     active: false
                 },
 
                 {
-                    img: '@/assets/img/buy-comics-digital-comics.png',
+                    img: require('@/assets/img/buy-comics-digital-comics.png'),
                     text: 'Dc merchandise',
                     url: '#',
                     active: true
                 },
 
                 {
-                    img: '@/assets/img/buy-comics-digital-comics.png',
+                    img: require('@/assets/img/buy-comics-digital-comics.png'),
                     text: 'subscription',
                     url: '#',
                     active: false
                 },
                 {
-                    img: '@/assets/img/buy-comics-digital-comics.png',
+                    img: require('@/assets/img/buy-comics-digital-comics.png'),
                     text: 'comic shop locator',
                     url: '#',
                     active: false
                 },
 
                 {
-                    img: '@/assets/img/buy-comics-digital-comics.png',
+                    img: require('@/assets/img/buy-comics-digital-comics.png'),
                     text: 'Games',
                     url: '#',
                     active: false
                 },
 
                 {
-                    img: '@/assets/img/buy-comics-digital-comics.png',
+                    img: require('@/assets/img/buy-comics-digital-comics.png'),
                     text: 'Collectibles',
                     url: '#',
                     active: false
                 },
 
                 {
-                    img: '@/assets/img/buy-comics-digital-comics.png',
+                    img: require('@/assets/img/buy-comics-digital-comics.png'),
                     text: 'dc power visa',
                     url: '#',
                     active: false
